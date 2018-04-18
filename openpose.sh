b9a2a55f5ef79f5cb1fbc3241ace3f37db055cfb
lanpartis <jiaoyongcheng@gmail.com>
2018-04-18 21:33:15 +0900

    openpose

---
+++ openpose.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+export LD_LIBRARY_PATH=~/openpose/3rdparty/caffe/build/lib:$LD_LIBRARY_PATH
+alias openpose='~/openpose/build/examples/openpose/openpose.bin' # set your own openpose directory
+dir='interaction_data'
+
+for folder in $(ls $dir); do
+    subfolder=${dir}"/$folder"
+    for action_clip in $(ls $subfolder);do 
+        echo $action_clip|grep -Eo '*[0-9]+.avi'|grep -Eo '[0-9]+'
+        newfolder=$(echo $action_clip|grep -Eo '*[0-9]+.avi'|grep -Eo '[0-9]+')
+        mkdir ${subfolder}"/$newfolder"
+        openpose --video ${subfolder}"/$action_clip" -write_json ${subfolder}"/$newfolder"
+    done
+done
+
