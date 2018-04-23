2e5dbe13972531164f51da1bcae38cb57ff14de3
lanpartis <jiaoyongcheng@gmail.com>
2018-04-23 16:29:04 +0900

    423-16

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Interaction_data_process
 Python script to process the recorded interaction video.
-## requirements:
+## Requirements:
 *pandas ffmpy openpose*
+## Usage 
+run
+
+python process_raw.py
+./openpose.sh
--- openpose.sh
+++ openpose.sh
@@ -6,8 +6,7 @@ dir='interaction_data'
 for folder in $(ls $dir); do
     subfolder=${dir}"/$folder"
     for action_clip in $(ls $subfolder);do 
-        echo $action_clip|grep -Eo '*[0-9]+.avi'|grep -Eo '[0-9]+'
-        newfolder=$(echo $action_clip|grep -Eo '*[0-9]+.avi'|grep -Eo '[0-9]+')
+        newfolder=$(echo $action_clip|grep -Eo '[0-9]+')
         mkdir ${subfolder}"/$newfolder"
         openpose --video ${subfolder}"/$action_clip" -write_json ${subfolder}"/$newfolder" --display 0 --render_pose 0
     done
