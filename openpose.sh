c22f05cb42c4f10f1dcfac28b98b8ec05892e784
lanpartis <jiaoyongcheng@gmail.com>
2018-05-05 15:14:08 +0900

    m

--- openpose.sh
+++ openpose.sh
@@ -7,8 +7,8 @@ for folder in $(ls $dir); do
     subfolder=${dir}"/$folder"
     for action_clip in $(ls $subfolder);do 
         newfolder=$(echo $action_clip|grep -Eo '[0-9]+')
-        mkdir ${subfolder}"/$newfolder"
-        openpose --video ${subfolder}"/$action_clip" -write_json ${subfolder}"/$newfolder" --display 0 --render_pose 0
+        #mkdir ${subfolder}"/$newfolder"
+        openpose --image_dir ${subfolder}"/$action_clip" -write_json ${subfolder}"/$action_clip" --display 0 --render_pose 0
     done
 done
 
--- process_raw.py
+++ process_raw.py
@@ -15,20 +15,24 @@ import ffmpy
 import pandas as pd
 import os
 import sys
+fps = 16
 video_dir = 'video_data'
 output_dir = 'interaction_data'
 label_dir = 'labeled_clips.data'
 label_table = pd.read_table(label_dir,encoding='utf-8',delim_whitespace=True,verbose=True)
 counter = {}
 for index,row in label_table.iterrows():
-    cmd = '-ss %d -t %d'%(row['start'],row['duration'])
+    cmd = '-r %d -ss %d -t %d'%(fps,row['start'],row['duration'])
     file_dir='%s/%s'%(video_dir,row['name'])
     out_dir='%s/%s'%(output_dir,row['label'])
     if not os.path.isdir(out_dir):
         os.mkdir(out_dir)
+    out_dir=out_dir+'/'+str(counter.get(row['label'],1))
+    if not os.path.isdir(out_dir):
+        os.mkdir(out_dir)
     clip=ffmpy.FFmpeg(
         inputs={file_dir:None},
-        outputs={out_dir+'/'+str(counter.get(row['label'],1))+'.mkv':cmd}
+        outputs={out_dir+'/'+'%03d.png':cmd}
     )
     counter[row['label']] = counter.get(row['label'],1)+1
     clip.run()
