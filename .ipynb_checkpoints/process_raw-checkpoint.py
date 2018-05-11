9c79b1713cd742481086592d451d1e5e638c18a8
lanpartis <jiaoyongcheng@gmail.com>
2018-05-11 17:12:16 +0900

    5.11

--- .ipynb_checkpoints/process_raw-checkpoint.py
+++ .ipynb_checkpoints/process_raw-checkpoint.py
@@ -36,25 +36,3 @@ for index,row in label_table.iterrows():
     )
     counter[row['label']] = counter.get(row['label'],1)+1
     clip.run()
-#split image
-root_dir='interaction_data/'
-categories = os.listdir(root_dir)
-# if 'talk' in categories:
-#     categories.remove('talk') 
-for cat in categories:
-    sub_dir = root_dir+cat+'/'
-    folders =os.listdir(sub_dir)
-    for f in folders:
-        file_dir = sub_dir+f+'/'
-        if os.path.isdir(file_dir):
-            frames = os.listdir(file_dir)
-            frame_keys=[]
-            for frame in frames:
-                if frame.endswith('png') and len(frame)==7:
-                    image_dir=file_dir+frame
-                    img = cv2.imread(image_dir)
-                    img_l=img[:,:int(img.shape[1]/2),:]
-                    img_r=img[:,int(img.shape[1]/2):,:]
-                    cv2.imwrite(file_dir+'%s_l.png'%frame[:3],img_l)
-                    cv2.imwrite(file_dir+'%s_r.png'%frame[:3],img_r)
-                    os.remove(image_dir)
